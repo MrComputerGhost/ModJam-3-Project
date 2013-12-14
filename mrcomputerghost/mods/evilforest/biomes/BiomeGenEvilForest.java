0d7ec39071d070c1383138ea2fb0d93ec65c5650
MrComputerGhost <mr.computerghost@gmail.com>
2013-12-14 08:33:03 -0800

    Updated Files

--- mrcomputerghost/mods/evilforest/biomes/BiomeGenEvilForest.java
+++ mrcomputerghost/mods/evilforest/biomes/BiomeGenEvilForest.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import java.util.Random;
 
 import mrcomputerghost.mods.evilforest.worldgen.WorldGenEvilForest1;
+import mrcomputerghost.mods.evilforest.worldgen.WorldGenEvilWell;
 import net.minecraft.block.Block;
 import net.minecraft.entity.monster.EntityCaveSpider;
 import net.minecraft.entity.monster.EntitySlime;
@@ -89,8 +90,8 @@ public class BiomeGenEvilForest extends BiomeGenBase
         {
             int k = par3 + par2Random.nextInt(16) + 8;
             int l = par4 + par2Random.nextInt(16) + 8;
-            WorldGenDesertWells worldgendesertwells = new WorldGenDesertWells();
-            worldgendesertwells.generate(par1World, par2Random, k, par1World.getHeightValue(k, l) + 1, l);
+            WorldGenEvilWell worldgenevilwell = new WorldGenEvilWell();
+            worldgenevilwell.generate(par1World, par2Random, k, par1World.getHeightValue(k, l) + 1, l);
         }
     }
 }
