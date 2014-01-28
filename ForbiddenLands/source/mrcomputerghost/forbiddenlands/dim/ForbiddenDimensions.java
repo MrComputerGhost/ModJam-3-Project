ca208c5149256e4e627a163461710e62812117e7
MrComputerGhost <mr.computerghost@gmail.com>
2014-01-27 16:25:10 -0800

    Clean Up Code

--- ForbiddenLands/source/mrcomputerghost/forbiddenlands/dim/ForbiddenDimensions.java
+++ ForbiddenLands/source/mrcomputerghost/forbiddenlands/dim/ForbiddenDimensions.java
@@ -5,11 +5,12 @@ import net.minecraftforge.common.DimensionManager;
 public class ForbiddenDimensions {
 
 	public static int dimID = -42;
-	
+
 	public static void initDims() {
-		DimensionManager.registerProviderType(dimID, WorldProviderForbidden2.class, true);
+		DimensionManager.registerProviderType(dimID,
+				WorldProviderForbidden2.class, true);
 		DimensionManager.registerDimension(dimID, dimID);
-		
+
 	}
-	
+
 }
